6b18d4fbf93513d3810ff7f9d8e69125b5375111
root <root@Leonardo-PC.localdomain>
2021-10-22 14:50:56 -0300

    Aula_4

--- env/Prod/main.tf
+++ env/Prod/main.tf
@@ -1,6 +1,10 @@
-module "aws-dev" {
+module "aws-prod" {
   source = "../../infra"
   instancia = "t2.micro"
   regiao_aws = "us-west-2"
   chave = "IaC-Prod"
+}
+
+output "IP" {
+  value = module.aws-prod.IP_publico
 }
