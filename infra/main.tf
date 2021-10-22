217f1579e1a0ac912c74cc62460602b0ae7f6f2a
root <root@Leonardo-PC.localdomain>
2021-10-22 14:49:15 -0300

    Aula_2

--- infra/main.tf
+++ infra/main.tf
@@ -11,21 +11,20 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = var.regiao_aws
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-03d5c68bab01f3496"
-  instance_type = "t2.micro"
-  key_name = "iac-alura"
-  # user_data = <<-EOF
-  #                #!/bin/bash
-  #                cd /home/ubuntu
-  #                echo "<h1>Feito com Terraform</h1>" > index.html
-  #                nohup busybox httpd -f -p 8080 &
-  #                EOF
+  instance_type = var.instancia
+  key_name = var.chave
   tags = {
     Name = "Terraform Ansible Python"
   }
 }
 
+resource "aws_key_pair" "chaveSSH" {
+  key_name = var.chave
+  public_key = file("${var.chave}.pub")
+  
+}
